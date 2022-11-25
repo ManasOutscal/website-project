054c178e36a66dc51111be758f580ba2e62aeb01
Muhammad Rizwan Joiya <muhammadrizwanjoiya444@gmail.com>
2022-11-25 20:13:43 +0500

    second commit

--- index.js
+++ index.js
@@ -10,55 +10,133 @@ app.use(express.static(publicpath));
 app.set("view engine","ejs")
 
 
-app.get("/jobs", async (req, resp) => {
-    let data = await Product.find({});
-    // let rdata = data.reduce((final,current)=>{
-    //     let obj = final.find((item) => item.City_Formula === current.City_Formula);
 
-    //     if (obj){
-    //         return final;
-    //     }
-    //     return final.concat([current])
-
-    // },[]);
-    let ddata = data.reduce((final,current)=>{
-        let obj = final.find((item) => item.City_Formula === current.City_Formula);
-
-        if (obj){
-            return final;
+app.get("/jobs/:job-jobs-in-:key",async (req,resp)=>{
+    let { limit = 10} =req.query;
+    let page = req.query.page ? Number(req.query.page) : 1;
+    let jobtitle= req.params.job;
+    let citytitle=req.params.key;
+    let rawdata = await Product.find(
+        {
+            "$or":[
+                {Department_with_Gaming:{$regex:req.params.job,$options:"i"},
+                City_Remote:{$regex:req.params.key,$options:"i"}
+                },
+                {Department_with_Gaming:{$regex:req.params.job,$options:"i"},
+                Company:{$regex:req.params.key,$options:"i"}
+                }
+            ]
         }
-        return final.concat([current])
-
-    },[]);
-    resp.render("jobss",{data,ddata})
+    );
+    let data = await Product.find(
+        {
+            "$or":[
+                {Department_with_Gaming:{$regex:req.params.job,$options:"i"},
+                City_Remote:{$regex:req.params.key,$options:"i"}
+                },
+                {Department_with_Gaming:{$regex:req.params.job,$options:"i"},
+                Company:{$regex:req.params.key,$options:"i"}
+                }
+            ]
+        }
+    ).limit(limit*1).skip((page - 1) * limit);
+    let data2 = await Product.find(
+        {
+            "$or":[
+                {City_Remote:{$regex:req.params.key,$options:"i"}},
+                {Company:{$regex:req.params.key,$options:"i"}}
+            ]
+        }
+    ).limit(limit*1).skip((page - 1) * limit);
+    let data3 = await Product.find(
+        {
+            "$or":[
+                {Job_title:{$regex:req.params.job,$options:"i"}}
+            ]
+        }
+    ).limit(limit*1).skip((page - 1) * limit);
+    let datanumber= rawdata.length;
+    let numberdata = (page - 1 ) * limit ;
+    let numberofPages = Math.ceil(datanumber / limit);
+    resp.render("jobss",{data,jobtitle,citytitle,datanumber,data2,data3,numberofPages,page,limit,numberdata})
 });
 
-app.get("/jobs/cities/:key",async (req,resp)=>{
-    let { page = 1,limit = 10} =req.query;
+app.get("/jobs/jobs-in-:key",async (req,resp)=>{
+    let { limit = 10} =req.query;
+    let page = req.query.page ? Number(req.query.page) : 1;
+    let jobtitle= "";
+    let citytitle=req.params.key;
+    let rawdata = await Product.find(
+        {
+            "$or":[
+                // {Position:{$regex:req.params.position}},
+                {Company:{$regex:req.params.key,$options:"i"}},
+                {City_Remote:{$regex:req.params.key,$options:"i"}}
+            ]
+        }
+    );
     let data = await Product.find(
         {
             "$or":[
                 // {Position:{$regex:req.params.position}},
-                {City_Formula:{$regex:req.params.key}}
+                {Company:{$regex:req.params.key,$options:"i"}},
+                {City_Remote:{$regex:req.params.key,$options:"i"}}
             ]
         }
     ).limit(limit*1).skip((page - 1) * limit);
-    resp.render("jobss",{data})
-
-});
-app.get("/jobs/companies/:key",async (req,resp)=>{
-    let { page = 1,limit = 10} =req.query;
-    let data = await Product.find(
+    let data2 = await Product.find(
         {
             "$or":[
                 // {Position:{$regex:req.params.position}},
-                {Company:{$regex:req.params.key}}
+                {Company:{$regex:req.params.key,$options:"i"}},
+                {City_Remote:{$regex:req.params.key,$options:"i"}}
             ]
         }
     ).limit(limit*1).skip((page - 1) * limit);
-    resp.render("jobss",{data})
+    let data3 = await Product.find(
+        {
+            "$or":[
+                // {Position:{$regex:req.params.position}},
+                {Company:{$regex:req.params.key,$options:"i"}},
+                {City_Remote:{$regex:req.params.key,$options:"i"}}
+            ]
+        }
+    ).limit(limit*1).skip((page - 1) * limit);
+    let datanumber= rawdata.length;
+    let numberdata = (page - 1 ) * limit ;
+    let numberofPages = Math.ceil(datanumber / limit);
+    resp.render("jobss",{data,jobtitle,citytitle,datanumber,data2,data3,numberofPages,page,limit,numberdata})
 
 });
 
+
+
+
+app.get("/jobs/page/:key",async (req,resp)=>{
+    let { page = 1,limit = 15} =req.query;
+    let data = await Product.find({_id:req.params.key});
+    let pobj = data[0];
+    
+    let data2 = await Product.find(
+        {
+            "$or":[
+                // {Position:{$regex:req.params.position}},
+                {City_Remote:{$regex:pobj.City_Remote,$options:"i"}
+                }
+            ]
+        }
+    ).limit(limit*1).skip((page - 1) * limit);
+    let data3 = await Product.find(
+        {
+            "$or":[
+                // {Position:{$regex:req.params.position}},
+                {Company:{$regex:pobj.Company,$options:"i"}
+                }
+            ]
+        }
+    ).limit(2*1).skip((page - 1) * 2);
+    resp.render("japplication",{pobj,data2,data3})
+
+});
 
 app.listen(port)
--- product.js
+++ product.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 const productSchema= mongoose.Schema({
-    Position:String,
+    Job_title:String,
+    Logo_link:String,
     Company:String,
-    Details:String,
-    City_Formula:String,
-    Position_Formula:String
+    Job_type:String,
+    Department_1:String,
+    Department_2:String,
+    Job_Description:String,
+    Senior_Junior:String,
+    City_Remote:String,
+    Department_with_Gaming:String
 });
 
 module.exports= mongoose.model("Job",productSchema);
